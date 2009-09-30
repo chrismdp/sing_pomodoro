c054b3f49253396f1f2bd2bbfdcddbec0ec53f00
Chris Parsons <chris@chris3.local>
2009-09-30 17:30:15 +0100

    Remove dodgy test migrate

--- lib/models.rb
+++ lib/models.rb
@@ -1,4 +1,5 @@
 def use_database(env)
+  puts("Using database: #{env}")
   db = File.dirname(__FILE__) + "/../config/database.yml"
   config = YAML.load(ERB.new(IO.read(db)).result)
   (config[env]).symbolize_keys
--- rakefile.rb
+++ rakefile.rb
@@ -27,7 +27,6 @@ task :default => :spec
 
 desc "Run the specs under spec/models"
 Spec::Rake::SpecTask.new do |t|
-  migrate("test")
   t.spec_opts = ['--options', "spec/spec.opts"]
   t.spec_files = FileList['spec/*_spec.rb']
 end
